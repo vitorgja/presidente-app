1ce1e1f54bcdd7805de80d3f4e96b31e2ba120a4
Vitor Pereira dos Santos <vitor_gja_@hotmail.com>
2018-09-18 14:21:56 +0000

    update .gitignore

--- src/pages/autenticacao/autenticacao.ts
+++ src/pages/autenticacao/autenticacao.ts
@@ -1,25 +1,49 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
-/**
- * Generated class for the AutenticacaoPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+import { Facebook } from '@ionic-native/facebook';
 
 @IonicPage()
 @Component({
   selector: 'page-autenticacao',
-  templateUrl: 'autenticacao.html',
+  templateUrl: 'autenticacao.html'
 })
+
 export class AutenticacaoPage {
-
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor() {
+    //atribuicao do pacote do facebook
+    public facebook: Facebook,
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad AutenticacaoPage');
+//método para chamar api do facebook e salvar no banco o usuario    
+loginFacebook() {
+     let permissions = new Array<string>();
+     permissions = ["public_profile", "email"];
+
+     this.facebook.login(permissions).then((response) => {
+      let params = new Array<string>();
+
+      this.facebook.api("/me?fields=name,email", params)
+      .then(res => {
+
+          //estou usando o model para criar os usuarios
+          let usuario = new Usuario();
+          usuario.nome = res.name;
+          usuario.email = res.email;
+          usuario.senha = res.id;
+          usuario.login = res.email;
+        
+          this.logar(usuario);
+      }, (error) => {
+        alert(error);
+        console.log('ERRO LOGIN: ',error);
+      })
+    }, (error) => {
+      alert(error);
+    });
   }
 
+  logar(usuario: Usuario) {
+    this.salvarService.salvarFacebook(usuario)
+    .then(() => {
+        console.log('Usuario cadastrado via facebook com sucesso!');
+    })
+  }
 }
